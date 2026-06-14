fd40c0d352022e5c2cc91587d42ac1f0ee9abee8
956237586 <master@hylstudio.cn>
2026-06-14 22:25:13 +0800

    0.2

--- run_kotlin_kernel_idea/run_kernel_idea.py
+++ run_kotlin_kernel_idea/run_kernel_idea.py
@@ -4,9 +4,7 @@ import sys
 from run_kotlin_kernel import run_kernel
 
 def run_kernel_idea_impl(connection_file: str, jar_args_file: str = None, executables_dir: str = None)->None:
-    abspath = os.path.abspath(__file__)
-    current_dir = os.path.dirname(abspath)
-    origin_kernel_dir = os.path.abspath(os.path.join(current_dir, '../run_kotlin_kernel'))
+    origin_kernel_dir = os.path.dirname(os.path.abspath(run_kernel.__file__))
     if jar_args_file is None:
         jar_args_file = os.path.join(origin_kernel_dir, 'config', 'jar_args.json')
     if executables_dir is None:
@@ -31,7 +29,11 @@ def run_kernel_idea_impl(connection_file: str, jar_args_file: str = None, execut
         payload = json.dumps(jar_args[1:])
         print('launch remote repl server, api: {}, args: {}'.format(start_kernel_api, payload))
         headers = {"Content-Type": "application/json"}
-        response = requests.post(start_kernel_api, data=payload, headers=headers)
+        try:
+            response = requests.post(start_kernel_api, data=payload, headers=headers, timeout=30)
+        except requests.exceptions.RequestException as e:
+            print('launch remote repl server failed: {}'.format(e))
+            return
         response_body = response.json()
         print('launch remote repl server reply code:{}, body: {}'.format(response.status_code, response_body))
         if response_body['code'] == 'S00000':
@@ -39,30 +41,29 @@ def run_kernel_idea_impl(connection_file: str, jar_args_file: str = None, execut
                 query_kernel_status_api = skykoma_agent_server_api_base + "/queryJupyterKernelStatus"
                 payload = json.dumps({})
                 headers = {"Content-Type": "application/json"}
-                # print('query remote reql server, api: {}, payload: {}'.format(query_kernel_status_api, payload))
-                response = requests.post(query_kernel_status_api, data=payload, headers=headers)
+                try:
+                    response = requests.post(query_kernel_status_api, data=payload, headers=headers, timeout=5)
+                except requests.exceptions.RequestException as e:
+                    print('query remote repl server failed: {}, retrying...'.format(e))
+                    time.sleep(1)
+                    continue
                 response_body = response.json()
                 if response_body['data'] == 'RUNNING':
                     time.sleep(1)
                 else:
                     print('remote repl server stoppted, code:{}, body: {}'.format(response.status_code, response_body))
                     break
+        print('Kernel interrupted, exiting')
+        os._exit(0)
+
 
 def run_kernel_idea(*args) -> None:
     print(args)
-    try:
-        skykoma_agent_idea = 'idea' in os.getenv('SKYKOMA_AGENT_TYPE', '')
-        if skykoma_agent_idea:
-            run_kernel_idea_impl(*args)
-        else:
-            run_kernel(*args)
-    except KeyboardInterrupt:
-        print('Kernel interrupted')
-        try:
-            sys.exit(130)
-        except SystemExit:
-            # noinspection PyProtectedMember,PyUnresolvedReferences
-            os._exit(130)
+    skykoma_agent_idea = 'idea' in os.getenv('SKYKOMA_AGENT_TYPE', '')
+    if skykoma_agent_idea:
+        run_kernel_idea_impl(*args)
+    else:
+        run_kernel(*args)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='run_kotlin_kernel_idea',  # 模块名称
-    version='0.1',                  # 版本号
+    version='0.2',                  # 版本号
     packages=find_packages(),       # 自动查找包
 )
